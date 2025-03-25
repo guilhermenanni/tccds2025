465182bb2a117f8f83bb869e50adc954c08a4787
joserenatokk <132354697+joserenatokk@users.noreply.github.com>
2025-03-25 11:03:34 -0300

    Update bancodedados.sql

--- bancodedados.sql
+++ bancodedados.sql
@@ -1,5 +1,5 @@
-create database db_aplicativo;
-use db_aplicativo;
+create database db_draftame;
+use db_draftame;
 
 CREATE TABLE tb_time(
 id_time INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -42,13 +42,29 @@ id_usuario INT NOT NULL,
 FOREIGN KEY(id_usuario) REFERENCES tb_usuario(id_usuario)
 );
 
+CREATE TABLE tb_resposta_postagem(
+id_resposta int auto_increment primary key,
+id_usuario int not null,
+id_postagem int not null,
+texto_resposta varchar(230), 
+foreign key (id_usuario) references tb_usuario(id_usuario),
+foreign key (id_postagem) references tb_postagem(id_postagem)
+);
+
+create table tb_chat(
+id_chat int auto_increment primary key,
+id_usuario int not null,
+texto_mensagem varchar(400),
+foreign key (id_usuario ) references tb_usuario(id_usuario)
+);
+
 /*
 
 OBSERVAÇOES
 
 o cnpj do time nao esta not null, tendo em vista que talves possamos ter times amadores
 ain, mas guilherme, eu nao gostei de tal coisa, FODASSE, O GIT HUB TA AQUI, MUDA, SEU ARROMBADO!!!!!(essa é pra tu japa)
-ta meio incompleto por que ainda ta faltando definir umas coisas, tipo, se vai ter char de texto, se as pessoas vao poder responder postagens e tals
+ta meio incompleto por que ainda ta faltando definir umas coisas, tipo, se vai ter char de texto, se as pessoas vao poder responder postagens e tals (sim, vai ter e eas pessoas podem)
 se eu esquecer de tirar isso aqui até outubro vai pega malzao
 
 
